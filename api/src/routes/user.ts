bca81c9131be813a3eb7bed6955c8d46c328b9f0
EmiCon97 <derleuchtturm97@gmail.com>
2022-05-24 18:10:50 -0300

    conflicts solved

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -6,17 +6,11 @@ router.get('/', (req,res) => {
     res.json("Holis")
 })
 
-<<<<<<< HEAD
 router.post('/user', async (req,res) => {
     let{name, lastName, password, dni, role} = req.body;
     try{
         let data = await SignIn(name, lastName, password, dni, role);
-=======
-router.post('/', async (req,res) => {
-    let{name, lastName, password, dni, role, environment} = req.body
-    try{
-        let data = await SignIn(name, lastName, password, dni, role, environment)
->>>>>>> 9558f6bd4b7acaf3b557941c98d58a4f9a55c649
+
         res.json(data)
     }catch(error){
         console.log(error);
