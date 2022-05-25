c655e310536b9ab1cee03fee90d5c289abfdc207
joseluiscrr <joseluiscriollojlcr@gmail.com>
2022-05-25 16:25:30 -0500

    Landing y organizando algunas cosas

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginButton from "./LoginButton";
+
+const Landing = () => {
+    return (
+        <div className="flex justify-center items-center w-full">
+            <LoginButton />
+        </div>
+    );
+};
+
+export default Landing;
