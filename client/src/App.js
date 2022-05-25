b4a5c213db6ca7c9ab29d9f7d542d7a9f3d7b56e
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-05-25 17:37:08 -0300

    creación de tablas con info de supervisores y empleados

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 
 import React from "react";
+import TableInfoSupervisors from "./components/TableInfoSupervisors";
 
 function App() {
     return (
-        <div className='container mx-auto bg-green-400 rounded-xl shadow border p-8 m-10'>
+        <div className='container max-w-5xl mx-auto bg-green-400 rounded-xl shadow border p-5 m-10'>
             <p className="text-3xl text-gray-700 font-bold mb-5">
                 Welcome!
             </p>
             <p className="text-gray-500 text-lg">
                 React and Tailwind CSS in action
             </p>
+            <TableInfoSupervisors/>
         </div>
     );
 };
