f0f75488f7b882414098e7b527d1a33bf89806c8
EEA94 <14elias06andrada94@gmail.com>
2022-05-28 19:23:21 -0300

    traducido a español y detalles de botones

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import { Route, Routes, BrowserRouter} from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import TableInfoSupervisors from "./components/supervisor/TableInfoSupervisors";
 import GuardProfile from "./components/guard/GuardProfile";
 import PerfilGuardia from "./components/PerfilGuardia";
 import AddUser from "./components/boss/AddUser";
-
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/' element={<AuthenticationBtn />} />
-        <Route path="/bossHome" element={<BossHome/>}/>
-        {/* <Route path="/bossHome/addUser" element={[<BossHome/>, <BossAddUser/>]}/>
-        <Route path="/bossHome/supervisors" element={[<BossHome/>, <TableInfoSupervisors/>]}/> */}
-        {/* <Route path="/bossHome" element={<BossHome/>}/> */}
-        <Route path="/BossNewUser" element={<BossAddUser/>}/>
-        <Route path='/TableInfoSupervisors' element={<TableInfoSupervisors />} />
-       
+        <Route path='/home' />
+        <Route path='/home/add' element={<AddUser />} />
+        <Route path='/home/supervisor' element={<TableInfoSupervisors />} />
+        <Route path='/guard/:id' element={<PerfilGuardia />}/>
         <Route path='/GuardProfile/:id' element={<GuardProfile />} />
       </Routes>
     </BrowserRouter>
