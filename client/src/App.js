00dfac56716837d5a58a75fde6f4615dc395c68c
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-05-31 21:03:10 -0300

    restricción de rutas en App

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,65 @@
 import React from "react";
-import { Route, Routes,BrowserRouter} from "react-router-dom";
+import { Route, Routes, BrowserRouter} from "react-router-dom";
 import NavBar from "./components/Navbar";
-import TableInfoSupervisors from "./components/supervisor/TableInfoSupervisors";
-import GuardProfile from "./components/guard/GuardProfile";
 import AddUser from "./components/boss/AddUser";
 import EditState from './components/EditState/EditState';
+import { UseProtectedRouteHome, UseProtectedRoutes} from "./customHooks/ProtectedRoutes";
+import GuardProfile from "./components/guard/GuardProfile";
 
 function App() {
+//objeto que simula datos del usuario logeado
+  let user = {
+    id: '629248c63b94e38d4f2a3790',
+    name: 'guardia',
+    roles: 'watcher',
+  }
+
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/home' />
+        <Route path="/" element={<h2>texto de prueba</h2>}/>
+        <Route path='/home' 
+          element={
+            <UseProtectedRouteHome 
+            user={user}
+            redirectPath='/'
+            roles={user ? user.roles : null}
+            />
+          }
+        />
+        <Route path='/home/add' 
+          element={
+            <UseProtectedRoutes
+              isAllowed={!!user && user.roles === "boss"}
+              children={<AddUser/>}
+            />
+          } 
+        />
+        <Route path='/EditState/:id' 
+          element={
+            <UseProtectedRoutes
+              isAllowed={!!user && user.roles === "watcher"}
+              children={<EditState />}
+            />        
+          } 
+        />
+        <Route path='/guard/:id' 
+          element={
+            <UseProtectedRoutes
+              isAllowed={!!user && user.roles === "watcher"}
+              children={<GuardProfile/>}
+            />
+          }
+        />
+
+
+
+        {/* <Route path='/home'/>
         <Route path='/home/add' element={<AddUser />} />
         <Route path='/home/supervisor' element={<TableInfoSupervisors />} />
         <Route path='/guard/:id' element={<GuardProfile />} />
-        <Route path='/EditState/:id' element={<EditState />} />
+        <Route path='/EditState/:id' element={<EditState />} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,22 @@
+import HomeBoss from "./boss/HomeBoss";
+import GuardProfile from "./guard/GuardProfile";
+import TableInfoSupervisors from "./supervisor/TableInfoSupervisors";
+
+export default function Home ({rol}) {
+    console.log(rol)
+    let home;
+    if (rol === "boss") {
+        home = <HomeBoss/>;
+    } else if (rol === "supervisor") {
+        home = <TableInfoSupervisors/>;
+    } else if (rol === "watcher") {
+        home = <GuardProfile/>;
+    }
+
+
+
+    return (
+        home
+    )
+    
+}
