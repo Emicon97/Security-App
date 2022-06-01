7fedc03f3945f1f4fa1b11479b5a770d9d8d1a4e
General <joseluiscrrof@gmail.com>
2022-05-31 21:19:34 -0500

    Estilos en Profile, suprimiendo estilos globales y customizando tailwind

--- client/src/components/reusable/Profile.jsx
+++ client/src/components/reusable/Profile.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Modal from "./Modal";
 import { getUsersById } from "../../redux/actions";
 import { useParams } from "react-router-dom";
+import { Primary } from "../styles/Buttons";
+import see from "../../assets/see.png";
 
 export default function UserProfile() {
   const dispatch = useDispatch();
@@ -20,62 +22,48 @@ export default function UserProfile() {
 
   return (
     <>
-      <div className="grid mt-6">
-        <div className="justify-around bg-[#EDF6FE] w-4/5 justify-self-center lg:flex">
-          <div className="flex-column justify-center m-4">
-            <img
-              src="https://thumbs.dreamstime.com/z/guardia-de-seguridad-90118429.jpg"
-              alt="foto de perfil"
-              width="300rem"
-              className="rounded-full overflow-hidden border-solid border-black"
-            />
-            <button
-              onClick={toggle}
-              className="rounded-lg border-solid border-2 border-black mr-2 hover:bg-cyan-200 m-4 py-1 px-4"
-            >
-              Profile Pic
-            </button>
+      <div className="h-4/5 flex justify-center items-center">
+        <div className="flex flex-col items-center m-4">
+          <img 
+            src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+            alt="foto de perfil"
+            width="300rem"
+            className={Image()}
+          />
+          <button onClick={toggle} className={Primary()}>
+            See pic
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+              <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>
+        <div className="bg-[#EEEDFF] w-2/5 h-64 p-5 m-4 h-60 rounded-3xl">
+          <div className="flex justify-end">
+            <button className={Primary()}>Edit</button>
           </div>
-          <div className="flex-column bg-gray-200 m-6 px-10 py-4 self-center rounded-2xl text-lg leading-10 lg:w-4/5">
-            <h2 className={text}>
-              {" "}
-              <span className={style}>Name: </span> {user.name}
-            </h2>
-            <h2 className={text}>
-              {" "}
-              <span className={style}>Last Name: </span> {user.lastName}{" "}
-            </h2>
-            <h2 className={text}>
-              {" "}
-              <span className={style}>Email: </span>{" "}
-              leomessielmasgrande@gmail.com
-            </h2>
-            <h2 className={text}>
-              {" "}
-              <span className={style}>DNI: </span> {user.dni}
-            </h2>
-            <h2 className={text}>
-              {" "}
-              <span className={style}>Phone Number: </span> +54 9 351-935-935
-            </h2>
-            <button className="rounded-lg border-solid border-2 border-black mr-2 hover:bg-cyan-200 px-5">
-              Edit
-            </button>
+          <div className="h-40 flex flex-col justify-end">
+            <p className="text-4xl font-semibold">Leonel Messi</p>
+            <p className="text-2xl italic font-light">leomessielmasgrande@gmail.com</p>
+            <p>123132342543513124</p>
+            <p>+54 9 351-935-935</p>
           </div>
         </div>
       </div>
       <Modal active={active} toggle={toggle}>
-        <div className="flex justify-between w-2/5">
-          <img
-            className="rounded-full overflow-hidden border-solid border-black justify-self-center"
-            src="https://thumbs.dreamstime.com/z/guardia-de-seguridad-90118429.jpg"
-            alt="Foto de perfil"
-          />
-        </div>
+        <img
+          className="w-80 h-80 rounded-full m-5"
+          src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+          alt="Foto de perfil"
+        />
       </Modal>
     </>
   );
-}
+};
 
-const style = `lg:text-xl text-gray-700 font-bold text-base`;
-const text = `text-sm lg:text-lg`;
+const Image = () => `
+  rounded-full 
+  border-4 border-white
+  ring-4 ring-[#0243EC]
+  m-5
+`;
