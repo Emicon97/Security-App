a24ff5dca3cccb113241ba997b63e5bf3fb0ec03
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-06-02 12:25:37 -0300

    archivo HomeBoss limpio

--- client/src/components/boss/HomeBoss.jsx
+++ client/src/components/boss/HomeBoss.jsx
@@ -1,13 +1,5 @@
 export default function HomeBoss() {
 
-
-//     const Image = () => `
-//     rounded-full 
-//     border-4 border-white
-//     ring-4 ring-[#0243EC]
-//     m-5
-//   `;
-
     return (
         <div></div>
     )
