393238f38d9f282b8003226b183dbb3399f4aca7
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-06-06 10:39:55 -0300

    renderizado de paginado ok

--- client/src/components/reusable/TableWithAddUser.jsx
+++ client/src/components/reusable/TableWithAddUser.jsx
@@ -4,10 +4,9 @@ import TableInfo from "../reusable/TableInfo";
 import { Primary } from "../styles/Buttons";
 
 export default function HomeBoss() {
-    const { id } = useParams();
     return (
         <div className='flex flex-col items-center justify-center'>
-            <TableInfo id={id} />
+            <TableInfo />
             <Link to="/user/add">
                 <button className={`mx-auto my-5 ${Primary()}`}>Add User</button>
             </Link>
