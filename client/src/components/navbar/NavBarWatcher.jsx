ba45dd34b19b637e7261827f59ae8c9105188958
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-06-07 23:11:03 -0300

    navbar watcher

--- client/src/components/navbar/NavBarWatcher.jsx
+++ client/src/components/navbar/NavBarWatcher.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-
-export default function NavBarSupervisor ({userData}) {
+export default function NavBarWatcher ({userData}) {
 
     return (
         <>
             {
                 userData
                 ? <li className="flex items-center gap-2.5">
-                    <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={`/supervisor/${userData._id}`}>
+                    <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={`/guard/${userData._id}`}>
                         Dashboard
                     </Link>
                     <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={`/user/${userData._id}/profile`}>
                         Perfil
-                    </Link>
-                    <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={`/user/${userData._id}`}>
-                        Empleados
-                    </Link>
-                    <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={`/user/${userData._id}`}>
-                        Asignar Tareas
                     </Link>
                     <Link className="p-2.5 hover:text-[#0243EC] focus:text-[#0243EC]" to={'/user/tasks'}>
                         Ver Tareas
