780ca3673bb9bb8b61534f465718adf8012a34d2
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-11 20:12:56 -0300

    no params

--- client/src/components/reusable/SeeInferiorTask.jsx
+++ client/src/components/reusable/SeeInferiorTask.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { getToDosById, getUsersById } from "../../redux/actions";
 import LoginController from "./LoginController";
 
 export default function SeeInferiorTask() {
     let dispatch = useDispatch();
-    const id = useParams();
+    const id = localStorage.getItem('id');
     const header = LoginController();
 
-                                  //a chequear si funca//
-    const userTasks = getToDosById(id, header)(dispatch);
     const userDetails = getUsersById(id, header)(dispatch);
 
     useEffect(() => {
