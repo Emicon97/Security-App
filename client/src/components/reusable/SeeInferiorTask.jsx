ad4697a3c4193bbdc7c3ae396b7605fb3a2dcec0
General <joseluiscrrof@gmail.com>
2022-06-14 17:07:41 -0500

    A medias SeeInferiorTasks

--- client/src/components/reusable/SeeInferiorTask.jsx
+++ client/src/components/reusable/SeeInferiorTask.jsx
@@ -12,7 +12,7 @@ import demo from "../../assets/demo.png";
 import { useLocation } from "react-router-dom";
 import "../styles/Loader.css"
 
-export default function SeeInferiorTask() {
+export default function SeeInferiorTask({show}) {
   const dispatch = useDispatch();
   const id = localStorage.getItem("id");
   const header = LoginController();
@@ -67,50 +67,56 @@ export default function SeeInferiorTask() {
     );
   } else {
     return (
-      <div>
-        {userDetails.profilePic && (
-          <div>
-            <img
-              src={userDetails.profilePic ? userDetails.profilePic : demo}
-              alt="Profile picture"
-              width="100rem"
-            />
-            <h3>
-              {" "}
-              {userDetails.name} {userDetails.lastName}{" "}
-            </h3>
-            <p>{userDetails.telephone}</p>
-            <p>{userDetails.email}</p>
-          </div>
-        )}
-        <div className="ml-80">
-          {userDetails.name && (
-            <h2>You are seeing {userDetails.name} tasks </h2>
-          )}
-          <ul className="mt-20">
-            {" "}
-            <br />
-            {userTasks.length ? (
-              userTasks.map((task) => (
-                <div key={task._id}>
-                  <li key={task._id}>
-                    {task.name} {task.priority} {task.status}
-                  </li>
-                  <button
-                    onClick={(e) => {
-                      reply_click(task._id);
-                      toggle();
-                    }}
-                  >
-                    See Reports
-                  </button>
+      <div className={`fixed top-16 right-0 bottom-0 ${show ? 'left-[245px]' : 'left-[87px]'} ease-in-out transition-all duration-700 overflow-auto font-['nunito']`}>
+          <div className="flex m-[10px] rounded-2xl bg-[#d9e7f5]">
+            <div className="w-[500px] h-[100px] ml-[15px] mr-auto my-[15px] border-r-2 border-r-white flex">
+              <div className="flex">
+                {
+                  userDetails.profilePic ? 
+                  <img src={userDetails.profilePic} alt="Profile picture" className="m-auto ring ring-[#0d2ec7] border-2 border-transparent h-[90px] w-[90px] rounded-full" /> :
+                  <svg xmlns="http://www.w3.org/2000/svg" className="m-auto ring ring-[#0d2ec7] border-2 border-transparent h-[90px] w-[90px] rounded-full" viewBox="0 0 20 20" fill="white">
+                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
+                  </svg>
+                }
+                <div className="p-[10px] pl-0 ml-2">
+                  <h3 className="text-3xl font-extrabold truncate flex">
+                    <p className="text-[#0d2ec7] mr-0.5">{userDetails.name.charAt(0).toUpperCase()}</p>{userDetails.name.slice(1)} {' '}
+                    {userDetails.lastName.charAt(0).toUpperCase() + userDetails.lastName.slice(1)}
+                  </h3>
+                  <p className="italic">{userDetails.email}</p>
+                  <p>{userDetails.telephone}</p>
                 </div>
-              ))
-            ) : (
-              <h2>This user has no tasks</h2>
-            )}
+              </div>
+            </div>
+              <h2 className="w-[45%] flex items-end text-3xl font-extrabold">
+                You are seeing<p className="text-[#0d2ec7] ml-1.5 mr-0.5">{userDetails.name.charAt(0).toUpperCase()}</p>{userDetails.name.slice(1)} tasks
+              </h2>
+          </div>
+        {/* <div className="ml-80">
+          <ul className="mt-20">
+            {
+              userTasks.length ? (
+                userTasks.map((task) => (
+                  <div key={task._id}>
+                    <li key={task._id}>
+                      {task.name} {task.priority} {task.status}
+                    </li>
+                    <button
+                      onClick={(e) => {
+                        reply_click(task._id);
+                        toggle();
+                      }}
+                    >
+                      See Reports
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <h2>This user has no tasks</h2>
+              )
+            }
           </ul>
-        </div>
+        </div> */}
         <Modal active={active} toggle={toggle}>
           <div>
             <h2>Reports of {editTask.name}</h2>
