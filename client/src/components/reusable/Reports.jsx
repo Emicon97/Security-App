3f0c6d1d2b92640d304e72ab674a6c4558a1bb65
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-14 01:41:40 -0300

    add employees with a nice font size

--- client/src/components/reusable/Reports.jsx
+++ client/src/components/reusable/Reports.jsx
@@ -26,7 +26,7 @@ export default function SentReports({show}) {
                   <h1>{report.title}</h1>
                   <h2>From You</h2>
                   <h2>To {report.receiver.lastName} {report.receiver.name}</h2>
-                  {console.log(report)}
+                  <img src={report.picture} alt="Report"></img>
                   <h3>{report.description}</h3>
                </>
             ))
