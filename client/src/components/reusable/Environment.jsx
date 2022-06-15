2cf6e7dccfa1d95db216498243b171cb7f53de39
General <joseluiscrrof@gmail.com>
2022-06-15 17:10:41 -0500

    Estilizando los environments

--- client/src/components/reusable/Environment.jsx
+++ client/src/components/reusable/Environment.jsx
@@ -13,8 +13,8 @@ export default function Environment({show}) {
    const header = LoginController();
    const user = localStorage.getItem('user');
    const id = localStorage.getItem('id');
-   console.log(user)
    const environmentUser = useSelector((state) => state.environmentUsers);
+   console.log(environmentUser);
    
    const [input, setInput] = useState({name: ""})
    const [errors, setErrors] = useState({})
@@ -58,53 +58,83 @@ export default function Environment({show}) {
    useEffect(() => {
       dispatch(getAllEnvironments(header));
    }, []);
+
    useEffect(()=>{
       dispatch(getEnvironmentUsers(id,name,header))
    },[id,name])
+
    return (
-
-      <div className={`fixed top-16 right-0 bottom-0 ${show ? "left-[245px]" : "left-[87px]"} ease-in-out transition-all duration-700`}>
-         { user === 'boss' ? <>
-         <form onSubmit={(e)=>handleSubmit(e)}>
-            <div>
-               <h2>Add New Enviroment</h2>
-            </div>
-            <div>
-               <label htmlFor="">Name:</label><input type="text" name="name" value={input.name} onChange={(e)=>handleChange(e)} />
-               {errors.name && <p>{errors.name}</p>}
-            </div>
-            <button
-               type="submit"
-                 
-               className={`${Primary()} m-auto`}
-               >
-               Add Environment
-            </button>
-         </form>
-         </>: null}
-         <div>
-            <label>
-               Environments:
-               <select onChange={handleChangeEnvironment}>
-                  <option value="select">Select Environment</option>
-                  {allEnvironments.length!==0?allEnvironments.map(e => (<option value={e.name}>{e.name}</option>)): null}
-               </select>
-            </label>
+      <div className={`fixed top-16 right-0 bottom-0 ${show ? "left-[245px]" : "left-[87px]"} ease-in-out transition-all duration-700 font-['nunito'] overflow-y-auto`}>
+         { 
+            user === 'boss' ? 
+            <>
+               <form onSubmit={(e)=>handleSubmit(e)} className="w-[60%] m-auto mt-[15px] mb-[15px] rounded-2xl p-[15px] shadow shadow-gray-300">
+                  <div className="flex flex-row justify-between">
+                     <h2 className="font-semibold flex"><p className="text-[#ff5cf4]">A</p>dd New Enviromen<p className="text-[#0023c4]">t</p></h2>
+                     <button title="Submit new environment" type="submit" className="h-5 w-5 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#0023c4">
+                           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        </svg>
+                     </button>
+                  </div>
+                  <div>
+                     <label htmlFor="" className="flex flex-row items-center font-medium">Name: {errors.name && <small className="text-red-500 italic ml-1 mt-[3px]">{errors.name}*</small>}</label>
+                     <input type="text" name="name" value={input.name} onChange={(e)=>handleChange(e)} placeholder="Name..." className={`${Input} w-[94%]`} />
+                  </div>
+               </form>
+            </> : 
+            null
+         }
+         <div className={`w-[60%] m-auto font-semibold flex flex-row items-center justify-between ${user !== 'boss' ? 'mt-[20px]' : ''}`}>
+            <label>Select:</label>
+            <select onChange={handleChangeEnvironment} className={`${Input('Select')} m-0`}>
+               <option value="select">Environment</option>
+               {allEnvironments.length!==0?allEnvironments.map(e => (<option value={e.name}>{e.name.charAt(0).toUpperCase() + e.name.slice(1)}</option>)): null}
+            </select>
          </div>
-         <div>
-            {
-               environmentUser.length !==0? environmentUser.map(e=> {
-                  return (<div>
-                        <h3>{e.name}</h3>
-                        {e.supervisor.length && user === 'boss' ? <p>Supervisores: </p>: null}
-                    {e.supervisor? e.supervisor.map(e => (<div><p>{e.name} {e.lastName}</p></div>)): null}
-                     {e.watcher.length ? <p>Watcher: </p>: null}
-                    {e.watcher?e.watcher.map(e => (<div><p>{e.name} {e.lastName}</p></div>)): "nada"}
-                      </div>)
-               }): null
-            }
-         </div>
+         {
+            environmentUser.length !== 0 ? 
+            environmentUser.map(e => {
+               return (
+                  <div className="w-[60%] m-auto rounded-2xl mt-[15px] p-[15px] shadow shadow-gray-300">
+                     <h3 className="flex">{e.name.charAt(0).toUpperCase() + e.name.slice(1)}:
+                        {
+                           user === 'boss' ?
+                           <><p className="italic text-[#ffbbc4] font-semibold mx-1 mr-2">Supervisors</p>and<p className="italic text-[#506be1] font-semibold mx-1">Watchers</p></> :
+                           <p className="italic text-[#506be1] font-semibold mx-1">Watchers</p>
+                        }
+                     </h3>
+                     <div className="flex flex-row justify-around text-white font-semibold">
+                        {
+                           user === 'boss' ?
+                           <div className="rounded-xl bg-[#fdced4] p-[5px] w-[48%] flex flex-col items-center">
+                              {
+                                 e.supervisor.length > 0 ?
+                                 e.supervisor.map(e => 
+                                    (<p className="truncate">{e.name.charAt(0).toUpperCase() + e.name.slice(1)} {e.lastName.charAt(0).toUpperCase() + e.lastName.slice(1)}</p>)
+                                 ) :
+                                 <div className="rounded-xl bg-[#fdced4] p-[5px] w-full flex items-center justify-center">You have no supervisors here</div>
+                              }
+                           </div> :
+                           null
+                        }
+                        {
+                           e.watcher.length > 0 ?
+                           <div className="rounded-xl bg-[#0023c480] p-[5px] w-[48%] flex flex-col items-center">
+                              {
+                                 e.watcher.map(e => 
+                                    (<p className="truncate">{e.name.charAt(0).toUpperCase() + e.name.slice(1)} {e.lastName.charAt(0).toUpperCase() + e.lastName.slice(1)}</p>)
+                                 )
+                              }
+                           </div> :
+                           <div className="rounded-xl bg-[#0023c480] p-[5px] w-[48%] flex items-center justify-center">You don't have watchers here</div>
+                        }
+                     </div>
+                  </div>
+               )
+            }) : 
+            null
+         }
       </div>
-
    ) 
 }
