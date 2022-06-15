7e52b9b33118410ec0b868b789e119c5fbf82ba7
General <joseluiscrrof@gmail.com>
2022-06-15 13:36:07 -0500

    Arreglando svg y show de Envionments

--- client/src/components/navbar/NavBarSupervisor.jsx
+++ client/src/components/navbar/NavBarSupervisor.jsx
@@ -49,13 +49,11 @@ export default function NavBarSupervisor ({userData, show}) {
                         </button>
                     </Link>
                     <Link className="flex w-full my-2" to={`/supervisor/${userData}/environment`}>
-                        <button className="flex focus:text-black hover:text-black cursor-pointer focus:outline-none font-['nunito'] font-extrabold text-[#cbcfdd]">
-                            <button className="hover:bg-[#0023c436] focus:bg-[#0023c436] rounded-full mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 focus:bg-[#0023c436]" viewBox="0 0 20 20" fill="#0023c4">
-                                    <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
-                                </svg>
-                            </button>
-                            <p className={`${show ? '' : 'opacity-0'} ease-in-out transition duration-1000 hover:duration-75`}>Environments</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#0023c4">
+                            <path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd" />
+                        </svg>
+                        <button className="focus:text-black hover:text-black cursor-pointer focus:outline-none font-extrabold text-[#cbcfdd] ml-1">
+                            <p className={`${show ? '' : 'opacity-0 ease-in-out transition duration-1000'}`}>Environments</p>
                         </button>
                     </Link>
                     <Link className="flex w-full my-2" to={`/supervisor/${userData}/reports/sender`}>
