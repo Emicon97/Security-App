1de5bf8509ba71d69f41e013c43427226563320e
Enzo <enz997.ing.ind@gmail.com>
2022-06-15 11:51:54 -0300

    environment

--- client/src/components/reusable/EnvironmentUsers.jsx
+++
@@ -1,8 +0,0 @@
-
-import { useDispatch, useSelector } from 'react-redux';
-
-export default function EnvironmentUsers({show}){
-    const dispatch = useDispatch();
-    const environmentUsers = useSelector((state)=>state.environmentUsers);
-    
-}
